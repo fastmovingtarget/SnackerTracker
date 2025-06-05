e883f56bc10ff7eae40ad2dfcba34a45e2c084d8
Target <fastmovingtarget220689@gmail.com>
2025-06-05 16:15:48 +0100

    Added and tested meal name form

--- Components/MealForm/MealForm.test.tsx
+++ Components/MealForm/MealForm.test.tsx
@@ -1,11 +1,33 @@
+//2025-06-05 : Added and tested meal name form
 //2025-06-04 : Initial Commit with placeholder implementation
 
-import { render, screen } from '@testing-library/react';
+import { render, userEvent } from '@testing-library/react-native';
 import MealForm from './MealForm';
 
 describe('MealForm Component', () => {
-  it('renders correctly', () => {
-    render(<MealForm />);
-    expect(screen.getByText('Meal Form')).toBeTruthy();
+  it('renders title text correctly', () => {
+    const {getByText} = render(<MealForm />);
+    expect(getByText('Meal Form')).toBeTruthy();
+  });
+  it("renders input field", () => {
+    const {getByText, getByLabelText} = render(<MealForm />);
+
+    expect(getByLabelText(/Meal Name Form/i)).toBeTruthy();
+    expect(getByLabelText(/Meal Name Form/i)).toHaveDisplayValue("");
+  })
+});
+describe('MealForm Component handles', () => {
+  it('input field changes', async () => {
+    const user = userEvent.setup();
+
+    const {getByLabelText} = render(<MealForm />);
+    const inputField = getByLabelText(/Meal Name Form/i);
+
+    // Simulate user typing into the input field
+    await user.press(inputField);
+    await user.type(inputField, 'Breakfast');
+    
+    // Check if the input field value has changed
+    expect(getByLabelText(/Meal Name Form/i)).toHaveDisplayValue("Breakfast");
   });
 });
--- Components/MealForm/MealForm.tsx
+++ Components/MealForm/MealForm.tsx
@@ -1,11 +1,17 @@
+//2025-06-05 : Added and tested meal name form
 //2025-06-04 : Initial Commit with placeholder implementation
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {ColumnContainer, RowContainer, PressableContainer, StyledText, StyledTextInput} from '../../CustomComponents/CustomComponents';
 
 export default function MealForm() {
     return (
-        <View>
-            <Text>Meal Form</Text>
-        </View>
+        <ColumnContainer>
+            <StyledText>Meal Form</StyledText>
+            <StyledTextInput 
+                aria-label='Meal Name Form'
+                placeholder='Enter Meal Name'
+            />
+        </ColumnContainer>
     );
 }
