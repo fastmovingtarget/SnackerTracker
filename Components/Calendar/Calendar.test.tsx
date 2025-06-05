c4e92073f71e7f4cc4b1d3712aa61e83d31f44fb
Target <fastmovingtarget220689@gmail.com>
2025-06-05 16:16:29 +0100

    Implementing and testing a full calendar visual

--- Components/Calendar/Calendar.test.tsx
+++ Components/Calendar/Calendar.test.tsx
@@ -1,11 +1,103 @@
+//2025-06-05 : Implementing and testing a full calendar visual
 //2025-06-04 : Initial Commit with placeholder implementation
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, userEvent }from '@testing-library/react-native';
 import Calendar from './Calendar';
 
-describe('Calendar Component', () => {
-  it('renders correctly', () => {
-    render(<Calendar />);
-    expect(screen.getByText('Calendar Component')).toBeTruthy();
-  });
+const mockDataContext = {
+};
+
+
+beforeEach(() => {
+  jest.resetAllMocks();
 });
+
+const monthsOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+describe("Recipe Plan Calendar Renders", () => {
+    test("The month correctly", () => {
+        const currentMonth = monthsOfYear[new Date().getMonth()];// can't hardcode this as it will change over time, but can at least check that it is the current month via a different method
+
+        const {getByText} = render(
+            <Calendar />
+        );
+
+        expect(getByText(`${currentMonth}`)).toBeTruthy();
+    });
+    test("Month navigation buttons", () => {
+        const {getByText} = render(
+            <Calendar />
+        );
+        
+        expect(getByText(/</i)).toBeTruthy();
+        expect(getByText(/>/i)).toBeTruthy();
+    });
+    test("The calendar day headers", () => {
+        const {getByText} = render(
+            <Calendar />
+        );
+
+        expect(getByText(/M/i)).toBeTruthy();
+        expect(getByText(/Tu/i)).toBeTruthy();
+        expect(getByText(/W/i)).toBeTruthy();
+        expect(getByText(/Th/i)).toBeTruthy();
+        expect(getByText(/F/i)).toBeTruthy();
+        expect(getByText(/Sa/i)).toBeTruthy();
+        expect(getByText(/Su/i)).toBeTruthy();
+        
+    });
+    test("The actual calendar itself", () => {
+        const today = new Date();
+        const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+
+        const {getAllByText} = render(
+            <Calendar />
+        );
+
+        for(let i = 1; i <= daysInMonth; i++) {
+            expect(getAllByText(`${i}`).length).toBeGreaterThan(0);// Check that each day of the month is rendered at least once
+        }        
+    });
+    test("The recipe names each day", () => {
+        
+    })
+});
+describe("Recipe Plan Calendar functionality", () => {
+    test("The month changes when the left button is pressed", async () => {
+        const user = userEvent.setup();
+        const {getByText, getAllByText} = render(
+            <Calendar />
+        );
+
+        const currentMonth = new Date().getMonth();
+        const previousMonth = new Date().setMonth(currentMonth - 1);
+
+        await user.press(getByText(/</i));
+
+        expect(getByText(new Date(previousMonth).toLocaleString('default', { month: 'short' }))).toBeTruthy();
+        const daysInMonth = new Date(new Date().getFullYear(), currentMonth, 0).getDate();
+
+        for(let i = 1; i <= daysInMonth; i++) {
+            expect(getAllByText(`${i}`).length).toBeGreaterThan(0);// Check that each day of the previous month is rendered
+        }
+    });
+
+    test("The month changes when the right button is pressed", async () => {
+        const user = userEvent.setup();
+        const {getByText, getAllByText} = render(
+            <Calendar />
+        );
+
+        const currentMonth = new Date().getMonth();
+        const nextMonth = new Date().setMonth(currentMonth + 1);
+
+        await user.press(getByText(/>/i));
+
+        expect(getByText(new Date(nextMonth).toLocaleString('default', { month: 'short' }))).toBeTruthy();
+        const daysInMonth = new Date(new Date().getFullYear(), currentMonth + 2, 0).getDate();
+
+        for(let i = 1; i <= daysInMonth; i++) {
+            expect(getAllByText(`${i}`).length).toBeGreaterThan(0);// Check that each day of the previous month is rendered
+        }
+    });
+})
--- Components/Calendar/Calendar.tsx
+++ Components/Calendar/Calendar.tsx
@@ -1,10 +1,61 @@
+//2025-06-05 : Implementing and testing a full calendar visual
 //2025-06-04 : Initial Commit with placeholder implementation
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import StyledText from '../../CustomComponents/StyledText';
+import ColumnContainer from '../../CustomComponents/ColumnContainer';
+import RowContainer from '../../CustomComponents/RowContainer';
+import PressableContainer from '../../CustomComponents/PressableContainer';
 
 export default function Calendar() {
+    
+    const [monthIndex, setMonthIndex] = useState(0); // 0 for current month, -1 for last month, 1 for next month
+
+    const daysOfWeek = ["M", "Tu", "W", "Th", "F", "Sa", "Su"];
+
+    const today = new Date();
+
+    const monthStartDate = new Date(today.getFullYear(), today.getMonth() + monthIndex, 1);
+
+    const daysArray = new Array(42).fill(null).map ((_, index) => {
+        return new Date(today.getFullYear(), today.getMonth() + monthIndex, (2 + index) - (monthStartDate.getDay() || 7));
+    })
+
     return (
-        <View>
-            <Text>Calendar Component</Text>
-        </View>
+        <ColumnContainer>
+            <RowContainer>
+                <PressableContainer onPress={() => setMonthIndex(monthIndex - 1)} style={{ paddingHorizontal: 10 }}>
+                    <StyledText>
+                        {'<'}
+                    </StyledText>
+                </PressableContainer>
+                    <StyledText style={{ flex: 1, textAlign: 'center' }}>
+                        {monthStartDate.toLocaleString('default', { month: 'short' })}
+                    </StyledText>
+                <PressableContainer onPress={() => setMonthIndex(monthIndex + 1)} style={{ paddingHorizontal: 10 }}>
+                    <StyledText >
+                        {'>'}
+                    </StyledText>
+                </PressableContainer>
+            </RowContainer>
+            <RowContainer>
+                {daysOfWeek.map((day, index) => (
+                    <RowContainer key={`day-header-${index}`} style={{ flex: 1, justifyContent: 'center' }}>
+                        <StyledText style={{ flex: 1, textAlign: 'center' }}>
+                            {day}
+                        </StyledText>
+                    </RowContainer>
+                ))}
+            </RowContainer>
+            <RowContainer style={{ flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                {daysArray.map((date, index) => (
+                    <ColumnContainer key={`date-${index}`} style={{width: '10%', padding:2, backgroundColor: '#000000'}}>
+                        <StyledText style={{ textAlign: 'center' }}>
+                            {date.getDate()}
+                        </StyledText>
+                    </ColumnContainer>
+                ))}
+            </RowContainer>
+        </ColumnContainer>
     );
 }
