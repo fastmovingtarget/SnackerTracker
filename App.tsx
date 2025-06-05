f8527c5f6ba88886dd6595451bb061212a05aab8
Target <fastmovingtarget220689@gmail.com>
2025-06-05 16:18:18 +0100

    Plugging the home page component into our main app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+//2025-06-05 : Plugging the home page component into our main app
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
