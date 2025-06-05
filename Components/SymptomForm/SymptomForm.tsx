5f389bd35af2125a966d4060dc0b0aec1d96346f
Target <fastmovingtarget220689@gmail.com>
2025-06-05 16:15:08 +0100

    Symptom form now actually has some text input, tested

--- Components/SymptomForm/SymptomForm.tsx
+++ Components/SymptomForm/SymptomForm.tsx
@@ -1,14 +1,24 @@
+//2025-06-05 : Symptom form now actually has some text input, tested
 //2025-06-04 : Initial Commit with placeholder implementation
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {ColumnContainer, RowContainer, PressableContainer, StyledText, StyledTextInput} from '../../CustomComponents/CustomComponents';
 
 export default function SymptomForm() {
     return (
-        <View >
-            <Text >Symptom Form</Text>
-            <Text >
-                This is a placeholder for the Symptom Form component.
-            </Text>
-        </View>
+        <ColumnContainer >
+            <StyledText >Symptom Form</StyledText>
+            <StyledTextInput 
+                aria-label='Symptom Name Form'
+                placeholder='Enter Symptom Name...'
+                style={{ marginBottom: 10 }} // Add some margin for better spacing
+            />
+            <StyledTextInput 
+                aria-label='Symptom Description Form'
+                placeholder='Enter Symptom Description...'
+                style={{ marginBottom: 10 }} // Add some margin for better spacing
+            />
+            
+        </ColumnContainer>
     );
 }
