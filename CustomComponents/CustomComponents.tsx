ab6bca1b1f1581e3fc7d11db9ec16c00e5fc33f7
Target <fastmovingtarget220689@gmail.com>
2025-06-05 16:14:22 +0100

    Unified import file for convenience

---
+++ CustomComponents/CustomComponents.tsx
@@ -0,0 +1,16 @@
+//2025-06-05 : Unified import file for convenience
+import ColumnContainer from "./ColumnContainer";
+import RowContainer from "./RowContainer";
+import PressableContainer from "./PressableContainer";
+import StyledText from "./StyledText";
+import ScrollableContainer from "./ScrollableContainer";
+import StyledTextInput from "./StyledTextInput";
+
+export {
+    ColumnContainer,
+    RowContainer,
+    PressableContainer,
+    StyledText,
+    ScrollableContainer,
+    StyledTextInput
+};
