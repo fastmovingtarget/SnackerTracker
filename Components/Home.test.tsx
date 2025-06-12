5fc7574650a9b3718398f237448dbdeca48ab7bb
Target <fastmovingtarget220689@gmail.com>
2025-06-12 12:40:45 +0100

    Tests for Calendar and Forms now visible in different states

--- Components/Home.test.tsx
+++ Components/Home.test.tsx
@@ -1,7 +1,8 @@
+//2025-06-12 : Tests for Calendar and Forms now visible in different states
 //2025-06-05 : Adding MealForm, SymptomForm and Calendar, collapsible buttons for the forms
 //2025-06-03 : Creating file and initialising a basic render
 import React from "react";
-import { Text } from "react-native";
+import { Text, Pressable } from "react-native";
 import { render, userEvent } from "@testing-library/react-native";
 import Home from "./Home";
 import MealForm from "./MealForm/MealForm";
@@ -31,7 +32,10 @@ beforeEach(() => {
   jest.resetAllMocks();
   (MealForm as jest.Mock).mockImplementation(() => <Text>Meal Form</Text>);
   (SymptomForm as jest.Mock).mockImplementation(() => <Text>Symptom Form</Text>);
-  (Calendar as jest.Mock).mockImplementation(() => <Text>Calendar</Text>);
+  (Calendar as jest.Mock).mockImplementation(({setSelectedDate, isVisible}) => 
+    <Pressable onPress={() => setSelectedDate(new Date())} style={{display: isVisible ? 'flex' : 'none'}}>
+      <Text>Calendar</Text>
+    </Pressable>);
 });
 
 describe("Home Component renders", () => {
@@ -39,23 +43,57 @@ describe("Home Component renders", () => {
     const { getByText } = render(<Home />);
     expect(getByText("Welcome to the Home Screen!")).toBeTruthy();
   });
-  it("Meal Form collapsible", () => {
-    const { getByText } = render(<Home />);
-    expect(getByText(/Meals/i)).toBeTruthy();
-  })
-  it("Symptom Form collapsible", () => {
-    const { getByText } = render(<Home />);
-    expect(getByText(/Symptoms/i)).toBeTruthy();
-  });
   it("Calendar element", () => {
     const { getByText } = render(<Home />);
     expect(getByText("Calendar")).toBeTruthy();
   });
 });
+describe("When a date is selected", () => {
+  it("Calendar hides", async () => {
+    const user = userEvent.setup();
+
+    const { getByText, queryByText } = render(<Home />);
+
+    expect(getByText("Calendar")).toBeTruthy();
+    await user.press(getByText("Calendar")); // Simulate selecting a date
+
+    expect(queryByText("Calendar")).toBeNull(); // Calendar should no longer be visible
+  });
+  it("Back to Calendar button appears", async () => {
+    const user = userEvent.setup();
+    const { getByText, queryByText } = render(<Home />);
+    
+    expect(getByText("Calendar")).toBeTruthy();
+    expect(queryByText("Back to Calendar")).toBeFalsy(); // Back to Calendar button should be visible
+
+    await user.press(getByText("Calendar")); // Simulate selecting a date
+    
+    expect(getByText("Back to Calendar")).toBeTruthy(); // Back to Calendar button should be visible
+  });
+  it("Back to Calendar button shows Calendar and hides Forms when pressed", async () => {
+    const user = userEvent.setup();
+    const { getByText, queryByText } = render(<Home />);
+
+    expect(getByText("Calendar")).toBeTruthy();
+    await user.press(getByText("Calendar")); // Simulate selecting a date
+
+    expect(queryByText("Calendar")).toBeNull(); // Calendar should no longer be visible
+    expect(getByText("Back to Calendar")).toBeTruthy(); // Back to Calendar button should be visible
+
+    await user.press(getByText("Back to Calendar")); // Simulate pressing Back to Calendar
+
+    expect(getByText("Calendar")).toBeTruthy(); // Calendar should now be visible again
+    expect(queryByText("Back to Calendar")).toBeFalsy(); // Back to Calendar button should be visible
+    expect(queryByText("Meals")).toBeFalsy(); // Back to Calendar button should be visible
+    expect(queryByText("Symptoms")).toBeFalsy(); // Back to Calendar button should be visible
+  })
+})
 describe("Home Component handles", () => {
   it("Meal Form toggle", async () => {
     const user = userEvent.setup();
     const { getByText, queryByText } = render(<Home />);
+
+    await user.press(getByText("Calendar")); // Simulate selecting a date
 
     expect(getByText(/Meals/i)).toBeTruthy();
     expect(queryByText("Meal Form")).toBeNull(); // Initially not displayed
@@ -68,6 +106,8 @@ describe("Home Component handles", () => {
     const user = userEvent.setup();
     const { getByText, queryByText } = render(<Home />);
 
+    await user.press(getByText("Calendar")); // Simulate selecting a date
+
     expect(getByText(/Symptoms/i)).toBeTruthy();
     expect(queryByText("Symptom Form")).toBeNull(); // Initially not displayed
 
@@ -78,6 +118,8 @@ describe("Home Component handles", () => {
   it("Collapsables toggle each other", async () => {
     const user = userEvent.setup();
     const { getByText, queryByText } = render(<Home />);
+
+    await user.press(getByText("Calendar")); // Simulate selecting a date
 
     expect(getByText(/Meals/i)).toBeTruthy();
     expect(getByText(/Symptoms/i)).toBeTruthy();
