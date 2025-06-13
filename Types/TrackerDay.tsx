9f37e9b48f3fdd12e4344ff54eca249ce65c58ed
Target <fastmovingtarget220689@gmail.com>
2025-06-13 10:32:19 +0100

    Meals and Symptoms are now the correct type

--- Types/TrackerDay.tsx
+++ Types/TrackerDay.tsx
@@ -1,7 +1,11 @@
+//2025-06-13 : Meals and Symptoms are now the correct type
 //2025-06-03 : Initialising basic type implementation
+import type Meal from "./Meal";
+import type Symptom from "./Symptom";
+
 export default interface TrackerDay {
     Date: string; // ISO date string
-    Meals: string[]; // Array of meal names
-    Symptoms: string[]; // Array of symptom names
+    Meals: Meal[]; // Array of meal names
+    Symptoms: Symptom[]; // Array of symptom names
     Notes?: string; // Optional notes for the day
 }
