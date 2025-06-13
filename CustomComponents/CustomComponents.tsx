d9acd86b5a9675f8cf30d6cba50921164548b302
Target <fastmovingtarget220689@gmail.com>
2025-06-13 10:33:35 +0100

    Streamlining the exports

--- CustomComponents/CustomComponents.tsx
+++ CustomComponents/CustomComponents.tsx
@@ -1,19 +1,10 @@
+//2025-06-13 : Streamlining the exports
 //2025-06-11 : Added Collapsible Container export
 //2025-06-05 : Unified import file for convenience
-import ColumnContainer from "./ColumnContainer";
-import RowContainer from "./RowContainer";
-import PressableContainer from "./PressableContainer";
-import StyledText from "./StyledText";
-import ScrollableContainer from "./ScrollableContainer";
-import StyledTextInput from "./StyledTextInput";
-import CollapsibleContainer from "./CollapsibleContainer";
-
-export {
-    ColumnContainer,
-    RowContainer,
-    PressableContainer,
-    StyledText,
-    ScrollableContainer,
-    StyledTextInput, 
-    CollapsibleContainer
-};
+export {default as ColumnContainer} from "./ColumnContainer";
+export {default as RowContainer} from "./RowContainer";
+export {default as PressableContainer} from "./PressableContainer";
+export {default as StyledText} from "./StyledText";
+export {default as ScrollableContainer} from "./ScrollableContainer";
+export {default as StyledTextInput} from "./StyledTextInput";
+export {default as CollapsibleContainer} from "./CollapsibleContainer";
