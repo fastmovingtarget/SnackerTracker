cf55842543374b13ce717167c7e01bc28427ce3e
Target <fastmovingtarget220689@gmail.com>
2025-08-19 14:48:19 +0100

    Date field type changed from String to Date

--- Types/TrackerDay.tsx
+++ Types/TrackerDay.tsx
@@ -1,10 +1,11 @@
+//2025-08-19 : Date field type changed from String to Date
 //2025-06-13 : Meals and Symptoms are now the correct type
 //2025-06-03 : Initialising basic type implementation
 import type Meal from "./Meal";
 import type Symptom from "./Symptom";
 
 export default interface TrackerDay {
-    Date: string; // ISO date string
+    Date: Date; // ISO date string
     Meals: Meal[]; // Array of meal names
     Symptoms: Symptom[]; // Array of symptom names
     Notes?: string; // Optional notes for the day
