554e32366ca7d0f8e8086c9dd0d0c813562f8a06
Target <fastmovingtarget220689@gmail.com>
2025-08-19 14:47:40 +0100

    Storage functions for accessing local storage

---
+++ Functions/StorageFunctions.tsx
@@ -0,0 +1,55 @@
+//2025-08-19 : Storage functions for accessing local storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type TrackerDay from '../Types/TrackerDay';
+import Storage from 'react-native-storage';
+
+const storage = new Storage({
+    // maximum capacity, default 1000 key-ids
+    size: 1,
+
+    // Use AsyncStorage for RN apps, or window.localStorage for web apps.
+    // If storageBackend is not set, data will be lost after reload.
+    storageBackend: AsyncStorage, // for web: window.localStorage
+
+    // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
+    // can be null, which means never expire.
+    defaultExpires: null,
+
+    // cache data in the memory. default is true.
+    enableCache: true,
+
+    // if data was not found in storage or expired data was found,
+    // the corresponding sync method will be invoked returning
+    // the latest data.
+    sync: {
+        // we'll talk about the details later.
+    }
+});
+
+export const writeToStorage = async (key: string, data: TrackerDay[]) => {
+    try {
+        await storage.save({
+            key: key, // The key for the data
+            data: data, // The data to be stored
+            expires: null // No expiration time
+        });
+    } catch (e) {
+        console.error("Error writing to storage:", e);
+    }
+}
+
+export const readFromStorage = async (key: string) => {
+
+    const resultPromise = new Promise((resolve, reject) => {
+        console.log("Reading from storage with key:", key);
+        storage.load({
+            key: key, // The key for the data
+        }).then((result) => {
+            resolve(result);
+        }).catch((err) => {
+            console.error("Error reading from storage:", err);
+            resolve(null); // Return null if there's an error
+        });
+    })
+    return resultPromise;
+}
