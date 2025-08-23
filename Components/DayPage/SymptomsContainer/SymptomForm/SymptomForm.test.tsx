2190e6546ac0d8eb2b619392552dbb4f3ec49a00
Target <fastmovingtarget220689@gmail.com>
2025-08-23 18:32:53 +0100

    Symptom form now contains only text input

---
+++ Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.test.tsx
@@ -0,0 +1,47 @@
+//2025-08-23 : Symptom form now contains only text input
+//2025-06-05 : Symptom form now actually has some text input, tested
+//2025-06-04 : Initial Commit with placeholder implementation
+import { render, userEvent } from '@testing-library/react-native';
+import SymptomForm from './SymptomForm';
+
+const testSymptom = { Symptom_Name: "Headache", Symptom_Description: "Mild headache in the evening" };
+
+describe('SymptomForm', () => {
+  it("renders symptom name text input", () => {
+    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+    expect(getByLabelText(/Symptom Name Input 1/i)).toBeTruthy();
+    expect(getByLabelText(/Symptom Name Input 1/i)).toHaveDisplayValue(testSymptom.Symptom_Name);
+  });
+  it.skip("renders symptom description text input", () => {
+    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+    expect(getByLabelText(/Symptom Description Input 1/i)).toBeTruthy();
+    expect(getByLabelText(/Symptom Description Input 1/i)).toHaveDisplayValue(testSymptom.Symptom_Description);
+  })
+
+})
+describe('SymptomForm handles', () => {
+  it('name input field changes', async () => {
+    const user = userEvent.setup();
+    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+    const symptomNameInput = getByLabelText(/Symptom Name Input 1/i);
+
+    // Simulate user typing into the symptom name input field
+    await user.press(symptomNameInput);
+    await user.type(symptomNameInput, 'Headache');
+    
+    // Check if the input field value has changed
+    expect(getByLabelText(/Symptom Name Input 1/i)).toHaveDisplayValue("HeadacheHeadache");
+  });
+  it.skip("description input field changes", async () => {
+    const user = userEvent.setup();
+    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+    const symptomDescriptionInput = getByLabelText(/Symptom Description Input 1/i);
+
+    // Simulate user typing into the symptom description input field
+    await user.press(symptomDescriptionInput);
+    await user.type(symptomDescriptionInput, 'Mild headache in the evening');
+
+    // Check if the input field value has changed
+    expect(getByLabelText(/Symptom Description Input 1/i)).toHaveDisplayValue("Mild headache in the evening");
+  });
+})
---
+++ Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.tsx
@@ -0,0 +1,32 @@
+//2025-08-23 : Symptom form now contains only text input
+//2025-06-05 : Symptom form now actually has some text input, tested
+//2025-06-04 : Initial Commit with placeholder implementation
+import React from 'react';
+import { RowContainer, EditableText} from '../../../../CustomComponents/CustomComponents';
+import type Symptom from '../../../../Types/Symptom';
+
+export default function SymptomForm({ submitHandler, symptom, index }: { submitHandler: (newSymptom: Symptom, index: number) => void, symptom: Symptom, index: number }) {
+
+    const [editedSymptom, setEditedSymptom] = React.useState<Symptom>(symptom);
+
+    const handleChangeText = (text: string) => {
+        setEditedSymptom({ ...editedSymptom, Symptom_Name: text });
+    };
+    
+    const handleFinishEditing = () => {
+        submitHandler(editedSymptom, index);
+    };
+
+    return (
+        <RowContainer>
+            <EditableText
+                text={symptom.Symptom_Name}
+                onChangeText={(text) => handleChangeText(text)}
+                onFinishEditing={handleFinishEditing}
+                containerStyle={{marginBottom: 10}}
+                aria-label={`Symptom Name Input ${index + 1}`}
+                placeholder='Enter Symptom Name...'
+            />
+        </RowContainer>
+    );
+}
