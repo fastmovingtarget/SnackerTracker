08906500573477d6ddc0bee99073bae1b88c0363
Target <fastmovingtarget220689@gmail.com>
2025-08-23 18:35:25 +0100

    Home now uses the DayPage container

--- Components/Home.test.tsx
+++ Components/Home.test.tsx
@@ -1,3 +1,4 @@
+//2025-08-23 : Home now uses the DayPage container
 //2025-08-19 : Added Loading and Saving data to local storage
 //2025-06-12 : Tests for Calendar and Forms now visible in different states
 //2025-06-05 : Adding MealForm, SymptomForm and Calendar, collapsible buttons for the forms
@@ -6,23 +7,17 @@ import React from "react";
 import { Text, Pressable } from "react-native";
 import { render, userEvent } from "@testing-library/react-native";
 import Home from "./Home";
-import MealForm from "./MealForm/MealForm";
-import SymptomForm from "./SymptomForm/SymptomForm";
 import Calendar from "./Calendar/Calendar";
+import DayPage from "./DayPage/DayPage";
 
-jest.mock("./MealForm/MealForm", () => {
-  return {
-    __esModule: true,
-    default: jest.fn()
-  }
-});
-jest.mock("./SymptomForm/SymptomForm", () => {
-  return {
-    __esModule: true,
-    default: jest.fn()
-  }
-});
 jest.mock("./Calendar/Calendar", () => {
+  return {
+    __esModule: true,
+    default: jest.fn()
+  }
+});
+
+jest.mock("./DayPage/DayPage", () => {
   return {
     __esModule: true,
     default: jest.fn()
@@ -31,16 +26,16 @@ jest.mock("./Calendar/Calendar", () => {
 
 beforeEach(() => {
   jest.resetAllMocks();
-  (MealForm as jest.Mock).mockImplementation(({ submitHandler }) => (
-      <Pressable onPress={() => {console.log("Meal Form Pressed");submitHandler("Meals", [{ Meal_Name: "Breakfast" }])}}>
-        <Text>Meal Form</Text>
-      </Pressable>
-    ));
-  (SymptomForm as jest.Mock).mockImplementation(() => <Text>Symptom Form</Text>);
-  (Calendar as jest.Mock).mockImplementation(({setSelectedDate, isVisible}) => 
-    <Pressable onPress={() => setSelectedDate(new Date())} style={{display: isVisible ? 'flex' : 'none'}}>
+  (Calendar as jest.Mock).mockImplementation(({ setSelectedDate }) => (
+    <Pressable onPress={() => { setSelectedDate(new Date("2025-08-19")); }}>
       <Text>Calendar</Text>
-    </Pressable>);
+    </Pressable>
+  ));
+  (DayPage as jest.Mock).mockImplementation(({ selectedDate, backHandler }) => (
+    <Pressable onPress={backHandler}>
+      <Text>{selectedDate?.Date.toLocaleDateString()}</Text>
+    </Pressable>
+  ));
 });
 
 describe("Home Component renders", () => {
@@ -64,81 +59,8 @@ describe("When a date is selected", () => {
 
     expect(queryByText("Calendar")).toBeNull(); // Calendar should no longer be visible
   });
-  it("Back to Calendar button appears", async () => {
-    const user = userEvent.setup();
-    const { getByText, queryByText } = render(<Home />);
-    
-    expect(getByText("Calendar")).toBeTruthy();
-    expect(queryByText("Back to Calendar")).toBeFalsy(); // Back to Calendar button should be visible
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-    
-    expect(getByText("Back to Calendar")).toBeTruthy(); // Back to Calendar button should be visible
-  });
-  it("Back to Calendar button shows Calendar and hides Forms when pressed", async () => {
-    const user = userEvent.setup();
-    const { getByText, queryByText } = render(<Home />);
-
-    expect(getByText("Calendar")).toBeTruthy();
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(queryByText("Calendar")).toBeNull(); // Calendar should no longer be visible
-    expect(getByText("Back to Calendar")).toBeTruthy(); // Back to Calendar button should be visible
-
-    await user.press(getByText("Back to Calendar")); // Simulate pressing Back to Calendar
-
-    expect(getByText("Calendar")).toBeTruthy(); // Calendar should now be visible again
-    expect(queryByText("Back to Calendar")).toBeFalsy(); // Back to Calendar button should be visible
-    expect(queryByText("Meals")).toBeFalsy(); // Back to Calendar button should be visible
-    expect(queryByText("Symptoms")).toBeFalsy(); // Back to Calendar button should be visible
-  })
 })
 describe("Home Component handles", () => {
-  it("Meal Form toggle", async () => {
-    const user = userEvent.setup();
-    const { getByText, queryByText } = render(<Home />);
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(getByText(/Meals/i)).toBeTruthy();
-    expect(queryByText("Meal Form")).toBeNull(); // Initially not displayed
-
-    await user.press(getByText(/Meals/i)); // Simulate press
-
-    expect(getByText("Meal Form")).toBeTruthy(); // Now displayed
-  });
-  it("Symptom Form toggle", async () => {
-    const user = userEvent.setup();
-    const { getByText, queryByText } = render(<Home />);
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(getByText(/Symptoms/i)).toBeTruthy();
-    expect(queryByText("Symptom Form")).toBeNull(); // Initially not displayed
-
-    await user.press(getByText(/Symptoms/i)); // Simulate press
-
-    expect(getByText("Symptom Form")).toBeTruthy(); // Now displayed
-  });
-  it("Collapsables toggle each other", async () => {
-    const user = userEvent.setup();
-    const { getByText, queryByText } = render(<Home />);
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(getByText(/Meals/i)).toBeTruthy();
-    expect(getByText(/Symptoms/i)).toBeTruthy();
-    expect(queryByText("Meal Form")).toBeNull(); // Initially not displayed
-    expect(queryByText("Symptom Form")).toBeNull(); // Initially not displayed
-
-    await user.press(getByText(/Meals/i)); // Simulate press on Meals
-    expect(getByText("Meal Form")).toBeTruthy(); // Now displayed
-    expect(queryByText("Symptom Form")).toBeNull(); // Symptom Form still not displayed
-
-    await user.press(getByText(/Symptoms/i)); // Simulate press on Symptoms
-    expect(getByText("Symptom Form")).toBeTruthy(); // Now displayed
-    expect(queryByText("Meal Form")).toBeNull(); // Meal Form should be hidden
-  })
   it("Submit Meal Form", async () => {
     const user = userEvent.setup();
     const mockSubmitHandler = jest.fn(() => console.log("Meal Form Submitted"));
--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,3 +1,4 @@
+//2025-08-23 : Home now uses the DayPage container
 //2025-08-19 : Added Loading and Saving data to local storage
 //2025-06-13 : Adding dummy function for meal form submit handling
 //2025-06-13 : Tidying up the styles
@@ -7,13 +8,10 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Calendar from './Calendar/Calendar';
-import { ColumnContainer, RowContainer, PressableContainer, StyledText, CollapsibleContainer, ScrollableContainer } from '../CustomComponents/CustomComponents';
-import MealForm from './MealForm/MealForm';
-import SymptomForm from './SymptomForm/SymptomForm';
-import type Meal from '../Types/Meal';
-import type Symptom from '../Types/Symptom';
+import { ColumnContainer, ScrollableContainer } from '../CustomComponents/CustomComponents';
 import type TrackerDay from '../Types/TrackerDay';
 import {writeToStorage, readFromStorage} from '../Functions/StorageFunctions';
+import DayPage from './DayPage/DayPage';
 
 export default function Home() {
 
@@ -38,32 +36,19 @@ export default function Home() {
   }, []);
 
 
-  const handleSubmit = (submitType : "Meals" | "Symptoms", submitArray : Meal[] | Symptom[]) => {    
+  const handleSubmit = (day : TrackerDay) => {
     const dateIndex = trackerData.findIndex((day : TrackerDay) => day.Date.valueOf() === selectedDate?.Date.valueOf());
 
     const newTrackerData = [...trackerData];
 
     if (dateIndex === -1) {
       // If the date does not exist, create a new TrackerDay
-      const newTrackerDay: TrackerDay = {
-        Date: selectedDate!.Date,
-        Meals: submitType === "Meals" ? (submitArray as Meal[]) : [],
-        Symptoms: submitType === "Symptoms" ? (submitArray as Symptom[]) : [],
-        Notes: ""
-      };
-      newTrackerData.push(newTrackerDay);
-      setSelectedDate(newTrackerDay); // Set the selected date to the new TrackerDay
+      newTrackerData.push(day);
+      setSelectedDate(day); // Set the selected date to the new TrackerDay
     }
     else{
-      // If the date exists, update the existing TrackerDay
-      const updatedTrackerDay = {...trackerData[dateIndex]};
-      if (submitType === "Meals") {
-        updatedTrackerDay.Meals = (submitArray as Meal[]);
-      } else if (submitType === "Symptoms") {
-        updatedTrackerDay.Symptoms = (submitArray as Symptom[]);
-      }
-      newTrackerData[dateIndex] = updatedTrackerDay;
-      setSelectedDate(updatedTrackerDay); // Update the selected date to the existing TrackerDay
+      newTrackerData[dateIndex] = day;
+      setSelectedDate(day); // Update the selected date to the existing TrackerDay
     }
     setTrackerData(newTrackerData);
     writeToStorage("trackerData", newTrackerData); // Save updated tracker data to storage
@@ -85,30 +70,7 @@ export default function Home() {
     <ScrollableContainer style={{borderRadius: 0, padding: 0, margin:0, justifyContent:"flex-start", flexDirection:"column", minWidth:"100%"}}>
       <Text style={{ textAlign:"center", color:"white"}}>Welcome to the Home Screen!</Text>
       <ColumnContainer style={{display: !selectedDate ? "none" : "flex", flex:1, justifyContent:"flex-start", alignItems:"flex-start"}}>
-        <PressableContainer
-          onPress={() => setSelectedDate(null)}
-          style={{alignSelf:"flex-start"}}>
-          <StyledText style={{textAlign: "center", fontSize: 18}}>
-            {"Back to Calendar"}
-          </StyledText>
-        </PressableContainer>
-        <StyledText style={{textAlign: "center", fontSize: 18}}>
-          {selectedDate ? `Selected Date: ${selectedDate.Date.toLocaleDateString()}` : "No date selected"}
-        </StyledText>
-        <CollapsibleContainer
-          collapsibleTitle="Meals"
-          isExpanded={focusForm === "Meal"}
-          onPress={() => setFocusForm(focusForm === "Meal" ? null : "Meal")}
-          >
-          <MealForm submitHandler={handleSubmit} existingMeals={selectedDate?.Meals}/>
-        </CollapsibleContainer>
-        <CollapsibleContainer
-          collapsibleTitle="Symptoms"
-          isExpanded={focusForm === "Symptom"}
-          onPress={() => setFocusForm(focusForm === "Symptom" ? null : "Symptom")}
-          >
-          <SymptomForm />
-        </CollapsibleContainer>
+        <DayPage selectedDate={selectedDate} backHandler={() => setSelectedDate(null)} handleSubmit={handleSubmit} />
       </ColumnContainer>
       <Calendar setSelectedDate={calendarDateSelectHandler} isVisible={!selectedDate}/>
     </ScrollableContainer>
