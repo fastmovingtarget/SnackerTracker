d4391e7bfa66bbd6696d52034143fb8dffabd039
Target <fastmovingtarget220689@gmail.com>
2025-08-25 17:18:28 +0100

    Displayed meals fixed to change when date is changed

--- Components/DayPage/MealsContainer/MealsContainer.tsx
+++ Components/DayPage/MealsContainer/MealsContainer.tsx
@@ -1,3 +1,4 @@
+//2025-08-25 : Displayed meals fixed to change when date is changed
 //2025-08-23 : Container for the meal input forms
 import Meal from '../../../Types/Meal';
 import React from 'react';
@@ -7,8 +8,14 @@ export default function MealsContainer({ meals, onSubmit }: { meals: Meal[], onS
 
     const [displayMeals, setDisplayMeals] = React.useState<Meal[]>([...meals, {Meal_Name: ''}]); // Add an empty meal for new entries
 
+    React.useEffect(() => {
+        setDisplayMeals([...meals, {Meal_Name: ''}]);
+    }, [meals]);
+
     const submitHandler = (newMeal: Meal, index: number) => {
         let updatedMeals = [...displayMeals];
+
+        newMeal.Meal_Ingredients = newMeal.Meal_Ingredients?.filter(ingredient => ingredient.Ingredient_Name.trim() !== ''); // Remove empty ingredients
 
         updatedMeals[index] = newMeal; // update the meal at the specified index
 
