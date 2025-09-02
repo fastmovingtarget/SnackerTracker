fdfeaee6ffc5b3d92e0894b8b6bb7f26638863e5
Target <fastmovingtarget220689@gmail.com>
2025-09-02 18:12:50 +0100

    Added today's meal/symptom input to home page

--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,3 +1,4 @@
+//2025-09-02 : Added today's meal/symptom input to home page
 //2025-08-28 : Added a clearStorage function for testing, removed error flag when empty
 //2025-08-26 : Day Page call now uses submitHandler rather than handleSubmit
 //2025-08-23 : Home now uses the DayPage container
@@ -10,15 +11,19 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Calendar from './Calendar/Calendar';
-import { ColumnContainer, ScrollableContainer } from '../CustomComponents/CustomComponents';
+import { ColumnContainer, RowContainer, ScrollableContainer } from '../CustomComponents/CustomComponents';
 import type TrackerDay from '../Types/TrackerDay';
 import {writeToStorage, readFromStorage, clearStorage} from '../Functions/StorageFunctions';
 import DayPage from './DayPage/DayPage';
+import MealsContainer from './DayPage/MealsContainer/MealsContainer';
+import SymptomsContainer from './DayPage/SymptomsContainer/SymptomsContainer';
+import type Meal from '../Types/Meal';
+import type Symptom from '../Types/Symptom';
 
 export default function Home() {
 
   const [trackerData, setTrackerData] = useState<TrackerDay[]>([]); // Placeholder for tracker data
-
+  const [todayData, setTodayData] = useState<TrackerDay | null>(null);
   const [selectedDate, setSelectedDate] = useState<TrackerDay | null>(null);
 
   useEffect(() => {
@@ -29,6 +34,17 @@ export default function Home() {
           ...day,
           Date: new Date(day.Date)// Ensure Date is a Date object
         }});
+        let todayDay : TrackerDay | undefined = parsedTrackerData.find(day => new Date(day.Date).toDateString() === new Date().toDateString())
+        if (!todayDay) {
+          todayDay = {
+            Date: new Date(),
+            Meals: [],
+            Symptoms: [],
+            Notes: ""
+          } as TrackerDay;
+          parsedTrackerData.push(todayDay);
+        }
+        setTodayData(todayDay);
         setTrackerData(parsedTrackerData);
       } else {
         setTrackerData([]); // Initialize with an empty array if no data is found
@@ -37,7 +53,7 @@ export default function Home() {
   }, []);
 
 
-  const handleSubmit = (day : TrackerDay) => {
+  const handleSubmit = (day : TrackerDay, fromHome : boolean = false) => {
     const dateIndex = trackerData.findIndex((day : TrackerDay) => day.Date.valueOf() === selectedDate?.Date.valueOf());
 
     const newTrackerData = [...trackerData];
@@ -45,12 +61,13 @@ export default function Home() {
     if (dateIndex === -1) {
       // If the date does not exist, create a new TrackerDay
       newTrackerData.push(day);
-      setSelectedDate(day); // Set the selected date to the new TrackerDay
     }
     else{
       newTrackerData[dateIndex] = day;
-      setSelectedDate(day); // Update the selected date to the existing TrackerDay
     }
+    if(!fromHome)
+      setSelectedDate(day); // Update the selected date to the existing TrackerDay
+
     setTrackerData(newTrackerData);
     writeToStorage("trackerData", newTrackerData); // Save updated tracker data to storage
   };
@@ -68,12 +85,32 @@ export default function Home() {
   }
 
   return (
-    <ScrollableContainer style={{borderRadius: 0, padding: 0, margin:0, justifyContent:"flex-start", flexDirection:"column", minWidth:"100%"}}>
+    <ScrollableContainer style={{borderRadius: 0, padding: 0, margin:0, justifyContent:"flex-start", flexDirection:"column", width:"100%"}}>
       <Text style={{ textAlign:"center", color:"white"}}>Welcome to the Home Screen!</Text>
       <ColumnContainer style={{display: !selectedDate ? "none" : "flex", flex:1, justifyContent:"flex-start", alignItems:"flex-start"}}>
         <DayPage selectedDate={selectedDate} backHandler={() => setSelectedDate(null)} submitHandler={handleSubmit} />
       </ColumnContainer>
-      <Calendar setSelectedDate={calendarDateSelectHandler} isVisible={!selectedDate}/>
+      <ColumnContainer style={{display: selectedDate ? "none" : "flex", flex:1, justifyContent:"flex-start", alignItems:"flex-start", width:"100%"}}>
+        <ScrollableContainer style={{justifyContent:"flex-start"}}>
+          <MealsContainer meals={todayData?.Meals || []} onSubmit={(submitArray : Meal[]) => {
+                  if(todayData === null) return;
+                  const updatedTrackerDay : TrackerDay = {
+                      ...todayData,
+                      Meals:[...submitArray]
+                  };
+                  handleSubmit(updatedTrackerDay, true);
+              }}/>
+          <SymptomsContainer symptoms={todayData?.Symptoms || []} onSubmit={(submitArray : Symptom[]) => {
+                  if(todayData === null) return;
+                  const updatedTrackerDay : TrackerDay = {
+                      ...todayData,
+                      Symptoms:[...submitArray]
+                  };
+                  handleSubmit(updatedTrackerDay, true);
+              }} />
+        </ScrollableContainer>
+        <Calendar setSelectedDate={calendarDateSelectHandler} isVisible={!selectedDate}/>
+      </ColumnContainer>
     </ScrollableContainer>
   );
 }
