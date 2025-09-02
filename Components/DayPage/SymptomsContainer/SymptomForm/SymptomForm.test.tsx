e649b6c38a108b8c22cdda1e1de0333274ef6cfb
Target <fastmovingtarget220689@gmail.com>
2025-09-02 18:09:37 +0100

    Added collapsible to description, moved blank name handling to container

--- Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.test.tsx
+++ Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.test.tsx
@@ -1,3 +1,4 @@
+//2025-09-02 : Added collapsible to description, moved blank name handling to container
 //2025-08-27 : Minor visual formatting changes
 //2025-08-26 : Added symptom description field back in
 //2025-08-23 : Symptom form now contains only text input
@@ -9,44 +10,121 @@ import SymptomForm from './SymptomForm';
 const testSymptom = { Symptom_Name: "Headache", Symptom_Description: "Mild headache in the evening" };
 
 describe('SymptomForm', () => {
-  it("renders symptom name text input", () => {
+  it("renders symptom name text", async () => {
+    const user = userEvent.setup();
     const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
 
-    expect(getByLabelText(/Symptom Name Input 1/i)).toBeTruthy();
-    expect(getByLabelText(/Symptom Name Input 1/i)).toHaveDisplayValue(testSymptom.Symptom_Name);
+    expect(getByLabelText(/Symptom Name 1 Text/i)).toBeTruthy();
+    expect(getByLabelText(/Symptom Name 1 Text/i)).toHaveTextContent("Headache");
   });
 
-  it("renders symptom description text input", () => {
-    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+  it("does not render symptom description text when collapsed", () => {
+    const { queryByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
 
-    expect(getByLabelText(/Symptom Description Input 1/i)).toBeTruthy();
-    expect(getByLabelText(/Symptom Description Input 1/i)).toHaveDisplayValue(testSymptom.Symptom_Description);
+    expect(queryByLabelText(/Symptom Description 1 Text/i)).toBeNull();
   })
-})
+});
 
-describe('SymptomForm handles', () => {
-  it('name input field changes', async () => {
+describe('Collapsible Sections', () => {
+  it("expands symptom description when symptom name is pressed", async () => {
     const user = userEvent.setup();
     const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
-    const symptomNameInput = getByLabelText(/Symptom Name Input 1/i);
 
-    // Simulate user typing into the symptom name input field
-    await user.press(symptomNameInput);
-    await user.type(symptomNameInput, 'Headache');
-    
-    // Check if the input field value has changed
-    expect(getByLabelText(/Symptom Name Input 1/i)).toHaveDisplayValue("HeadacheHeadache");
+    await user.press(getByLabelText(/Symptom Name 1 Text/i));
+
+    expect(getByLabelText(/Symptom Description 1 Text/i)).toBeTruthy();
   });
-  it("description input field changes", async () => {
+
+  it("collapses symptom description when symptom name is pressed", async () => {
     const user = userEvent.setup();
-    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
-    const symptomDescriptionInput = getByLabelText(/Symptom Description Input 1/i);
+    const { getByLabelText, queryByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
 
-    // Simulate user typing into the symptom description input field
-    await user.press(symptomDescriptionInput);
-    await user.type(symptomDescriptionInput, 'Mild headache in the evening');
+    await user.press(getByLabelText(/Symptom Name 1 Text/i));
 
-    // Check if the input field value has changed
-    expect(getByLabelText(/Symptom Description Input 1/i)).toHaveDisplayValue("Mild headache in the eveningMild headache in the evening");
+    expect(getByLabelText(/Symptom Description 1 Text/i)).toBeTruthy();
+
+    await user.press(getByLabelText(/Symptom Name 1 Text/i));
+
+    expect(queryByLabelText(/Symptom Description 1 Text/i)).toBeNull();
   });
-})
+
+  it("renders new description input when expanded and symptom description is empty", async () => {
+    const user = userEvent.setup();
+    const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={{ Symptom_Name: "Headache", Symptom_Description: "" }} index={0} />);
+
+    await user.press(getByLabelText(/Symptom Name 1 Text/i));
+
+    expect(getByLabelText(/Symptom Description 1 Input/i)).toBeTruthy();
+  });
+  it("Submits new description input when expanded and symptom description is empty", async () => {
+    const user = userEvent.setup();
+    const submitHandler = jest.fn();
+    const { getByLabelText } = render(<SymptomForm submitHandler={submitHandler} symptom={{ Symptom_Name: "Headache", Symptom_Description: "" }} index={0} />);
+
+    await user.press(getByLabelText(/Symptom Name 1 Text/i));
+    await user.type(getByLabelText(/Symptom Description 1 Input/i), "New description");
+
+    expect(submitHandler).toHaveBeenCalledWith({ Symptom_Name: "Headache", Symptom_Description: "New description" }, 0);
+  });
+});
+
+describe('Editable Text', () => {
+  describe('name field', () => {
+    it('transitions from text to input', async () => {
+      const user = userEvent.setup();
+      const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+
+      await user.longPress(getByLabelText(/Symptom Name 1 Text/i));
+      expect(getByLabelText(/Symptom Name 1 Input/i)).toBeTruthy();
+    });
+    it('displays correct initial value when editing', async () => {
+      const user = userEvent.setup();
+      const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+
+      await user.longPress(getByLabelText(/Symptom Name 1 Text/i));
+
+      expect(getByLabelText(/Symptom Name 1 Input/i)).toHaveDisplayValue("Headache");
+    });
+    it('Finishes editing correctly', async () => {
+      const user = userEvent.setup();
+      const submitHandler = jest.fn();
+      const { getByLabelText } = render(<SymptomForm submitHandler={submitHandler} symptom={testSymptom} index={0} />);
+
+      await user.longPress(getByLabelText(/Symptom Name 1 Text/i));
+
+      await user.type(getByLabelText(/Symptom Name 1 Input/i), ' and Vomiting');
+
+      expect(submitHandler).toHaveBeenCalledWith({ Symptom_Name: "Headache and Vomiting", Symptom_Description: "Mild headache in the evening" }, 0);
+    });
+  })
+  describe('description field', () => {
+    it('transitions from text to input', async () => {
+      const user = userEvent.setup();
+      const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+
+      await user.press(getByLabelText(/Symptom Name 1 Text/i));
+      await user.press(getByLabelText(/Symptom Description 1 Text/i));
+      expect(getByLabelText(/Symptom Description 1 Input/i)).toBeTruthy();
+    });
+    it('displays correct initial value', async () => {
+      const user = userEvent.setup();
+      const { getByLabelText } = render(<SymptomForm submitHandler={jest.fn()} symptom={testSymptom} index={0} />);
+
+      await user.press(getByLabelText(/Symptom Name 1 Text/i));
+      await user.press(getByLabelText(/Symptom Description 1 Text/i));
+
+      expect(getByLabelText(/Symptom Description 1 Input/i)).toHaveDisplayValue("Mild headache in the evening");
+    });
+    it('finishes editing correctly', async () => {
+      const user = userEvent.setup();
+      const submitHandler = jest.fn();
+      const { getByLabelText } = render(<SymptomForm submitHandler={submitHandler} symptom={testSymptom} index={0} />);
+
+      await user.press(getByLabelText(/Symptom Name 1 Text/i));
+      await user.press(getByLabelText(/Symptom Description 1 Text/i));
+      await user.type(getByLabelText(/Symptom Description 1 Input/i), ' and Nausea');
+
+      expect(submitHandler).toHaveBeenCalledWith({ Symptom_Name: "Headache", Symptom_Description: "Mild headache in the evening and Nausea" }, 0);
+    });
+  });
+});
--- Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.tsx
+++ Components/DayPage/SymptomsContainer/SymptomForm/SymptomForm.tsx
@@ -1,41 +1,80 @@
+//2025-09-02 : Added collapsible to description, moved blank name handling to container
 //2025-08-26 : Added symptom description field back in
 //2025-08-23 : Symptom form now contains only text input
 //2025-06-05 : Symptom form now actually has some text input, tested
 //2025-06-04 : Initial Commit with placeholder implementation
-import React from 'react';
-import { RowContainer, EditableText, ColumnContainer} from '../../../../CustomComponents/CustomComponents';
+import React, {useEffect, useState} from 'react';
+import { Pressable } from 'react-native';
+import { RowContainer, EditableText, ColumnContainer, StyledText, StyledTextInput} from '../../../../CustomComponents/CustomComponents';
 import type Symptom from '../../../../Types/Symptom';
 
 export default function SymptomForm({ submitHandler, symptom, index }: { submitHandler: (newSymptom: Symptom, index: number) => void, symptom: Symptom, index: number }) {
 
-    const [editedSymptom, setEditedSymptom] = React.useState<Symptom>(symptom);
+    const [isEditing, setIsEditing] = useState<"Name" | "Description" | null>(null);
+    const [editedSymptom, setEditedSymptom] = useState<Symptom>({ ...symptom });
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const [symptomNameAutoFocus, setSymptomNameAutoFocus] = useState<boolean>(false);
+    const rotateString = isExpanded ? "90deg" : "0deg";
 
-    const handleChangeText = (text: string) => {
-        setEditedSymptom({ ...editedSymptom, Symptom_Name: text });
+    useEffect(() => {
+            setEditedSymptom({ ...symptom });
+    }, [symptom]);
+
+    const handleFinishEditingName = (text: string) => {
+        setIsEditing(null);
+        submitHandler({...editedSymptom, Symptom_Name: text }, index);
     };
-    
-    const handleFinishEditing = () => {
-        submitHandler(editedSymptom, index);
+    const handleFinishEditingDescription = (text: string) => {
+        setIsEditing(null);
+        submitHandler({...editedSymptom, Symptom_Description: text }, index);
     };
 
     return (
         <ColumnContainer>
-            <EditableText
-                text={symptom.Symptom_Name}
-                onChangeText={(text) => handleChangeText(text)}
-                onFinishEditing={handleFinishEditing}
-                containerStyle={{marginBottom: 10}}
-                aria-label={`Symptom Name Input ${index + 1}`}
-                placeholder='Enter Symptom Name...'
-            />
-            <EditableText
-                text={symptom?.Symptom_Description || ""}
-                onChangeText={(text) => handleChangeText(text)}
-                onFinishEditing={handleFinishEditing}
-                containerStyle={{marginBottom: 10}}
-                aria-label={`Symptom Description Input ${index + 1}`}
-                placeholder='Enter Symptom Description...'
-            />
+            <Pressable onPress={() => setIsExpanded(!isExpanded)} onLongPress={() => {
+                setIsEditing("Name");
+                setSymptomNameAutoFocus(true);
+            }}>
+                <RowContainer>
+                    {editedSymptom.Symptom_Name !== "" ? 
+                        <StyledText style={{fontSize:20, fontWeight:"bold", transform:[{rotate:rotateString}], paddingVertical:0}}>{"\u203A"}</StyledText> : 
+                        null}
+                    <EditableText
+                        text={editedSymptom.Symptom_Name}
+                        onFinishEditing={handleFinishEditingName}
+                        containerStyle={{marginBottom: 10}}
+                        aria-label={`Symptom Name ${index + 1}`}
+                        placeholder='Enter Symptom Name...'
+                        isEditing={isEditing === "Name"}
+                        autoFocus={symptomNameAutoFocus}
+                    />
+                </RowContainer>
+            </Pressable>
+            {!isExpanded ? null 
+            : (editedSymptom.Symptom_Description) ? (
+            <Pressable onPress={() => setIsEditing("Description")}>
+                <RowContainer>
+                    <EditableText
+                        text={editedSymptom.Symptom_Description}
+                        onFinishEditing={handleFinishEditingDescription}
+                        containerStyle={{marginBottom: 10}}
+                        aria-label={`Symptom Description ${index + 1}`}
+                        placeholder='Enter Symptom Description...'
+                        isEditing={isEditing === "Description"}
+                    />
+                </RowContainer>
+            </Pressable>
+            ) : (
+            <RowContainer>
+                <StyledTextInput
+                    defaultValue={""}
+                    onFinishEditing={handleFinishEditingDescription}
+                    placeholder='Enter Symptom Description...'
+                    aria-label={`Symptom Description ${index + 1} Input`}
+                />
+            </RowContainer>
+            )}
+            
         </ColumnContainer>
     );
 }
