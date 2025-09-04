f8b4b640b3d96c50c1a6547435dca2d66cbc9db5
Target <fastmovingtarget220689@gmail.com>
2025-09-04 12:51:56 +0100

    Moving Tracker load and submit logic to Tracker Context, unifying day page and calendar

--- Components/Home.test.tsx
+++ Components/Home.test.tsx
@@ -1,3 +1,4 @@
+//2025-09-04 : Moving Tracker load and submit logic to Tracker Context, unifying day page and calendar
 //2025-08-26 : Changed tests to reflect new design for home page
 //2025-08-23 : Home now uses the DayPage container
 //2025-08-19 : Added Loading and Saving data to local storage
@@ -10,15 +11,7 @@ import { render, userEvent, waitFor } from "@testing-library/react-native";
 import Home from "./Home";
 import Calendar from "./Calendar/Calendar";
 import DayPage from "./DayPage/DayPage";
-import {writeToStorage, readFromStorage} from '../Functions/StorageFunctions';
 
-jest.mock('../Functions/StorageFunctions', () => {
-  return {
-    __esModule: true,
-    writeToStorage: jest.fn(),
-    readFromStorage: jest.fn()
-  }
-});
 
 jest.mock("./Calendar/Calendar", () => {
   return {
@@ -45,91 +38,32 @@ const mockSubmitHandler = jest.fn();
 
 beforeEach(() => {
   jest.resetAllMocks();
-  (Calendar as jest.Mock).mockImplementation(({ setSelectedDate, isVisible }) => (
-    <Pressable onPress={() => { setSelectedDate(new Date("2025-08-19")); }} style={{ display: isVisible ? 'flex' : 'none' }}>
+  (Calendar as jest.Mock).mockImplementation(() => (
+    <Pressable>
       <Text>Calendar</Text>
     </Pressable>
   ));
-  (DayPage as jest.Mock).mockImplementation(({ selectedDate, backHandler, submitHandler }) => (
+  (DayPage as jest.Mock).mockImplementation(() => (
     <>
-      <Pressable onPress={backHandler}>
-        <Text>{selectedDate?.Date.toLocaleDateString()}</Text>
-      </Pressable>
-      <Pressable onPress={() => mockSubmitHandler(testTrackerDay)}>
+      <Pressable>
         <Text>Tracker Day</Text>
       </Pressable>
     </>
   ));
-  (readFromStorage as jest.Mock).mockResolvedValue([testTrackerDay]);
 });
 
 describe("Home Component renders", () => {
-  it("Title Text",  async () => {
-    const { getByText } = render(<Home />);
-    await waitFor(() => {
-      expect(getByText("Welcome to the Home Screen!")).toBeTruthy();
-    });
-  });
   it("Calendar element",  async () => {
     const { getByText } = render(<Home />);
-    await waitFor(() => {
-      expect(getByText("Calendar")).toBeTruthy();
-    });
-  });
-});
-describe("When a date is selected", () => {
-  it("Calendar hides", async () => {
-    const user = userEvent.setup();
-
-    const { getByText, queryByText } = render(<Home />);
 
     expect(getByText("Calendar")).toBeTruthy();
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(queryByText("Calendar")).toBeNull(); // Calendar should no longer be visible
   });
-  it("DayPage appears with correct date", async () => {
+  it("DayPage", async () => {
     const user = userEvent.setup();
 
     const { getByText } = render(<Home />);
 
-    await user.press(getByText("Calendar")); // Simulate selecting a date
 
-    expect(getByText(new Date("2025-08-19").toLocaleDateString())).toBeTruthy(); // DayPage should display the selected date
+    expect(getByText("Tracker Day")).toBeTruthy(); // DayPage should display the selected date
   });
-  it("When backHandler is called, should return to calendar view", async () => {
-    const user = userEvent.setup();
-
-    const { getByText, queryByText } = render(<Home />);
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    expect(queryByText(new Date("2025-08-19").toLocaleDateString())).toBeTruthy(); // DayPage should display the selected date
-
-    await user.press(getByText(new Date("2025-08-19").toLocaleDateString())); // Simulate pressing back
-
-    expect(queryByText("Calendar")).toBeTruthy(); // Calendar should be visible again
-  });
-})
-describe("Home Component handles", () => {
-  it("Submit Meal Form", async () => {
-    const user = userEvent.setup();
-    const testAgainstTrackerDay = {
-      Date: new Date("2025-08-19"),
-      Meals: [{ Meal_Name: "Breakfast" }],
-      Symptoms: [],
-      Notes: ""
-    };
-
-    const { getByText } = render(<Home />);
-
-    await user.press(getByText("Calendar")); // Simulate selecting a date
-
-    const trackerDateText = getByText(/Tracker Day/i);
-    expect(trackerDateText).toBeTruthy();
-
-    await user.press(getByText(/Tracker Day/i)); // Submit The tracker day
-
-    expect(mockSubmitHandler).toHaveBeenCalledWith(testAgainstTrackerDay);
-  });
-})
+});
--- Components/Home.tsx
+++ Components/Home.tsx
@@ -1,3 +1,4 @@
+//2025-09-04 : Moving Tracker load and submit logic to Tracker Context, unifying day page and calendar
 //2025-09-02 : Added today's meal/symptom input to home page
 //2025-08-28 : Added a clearStorage function for testing, removed error flag when empty
 //2025-08-26 : Day Page call now uses submitHandler rather than handleSubmit
@@ -8,110 +9,16 @@
 //2025-06-12 : Moving Calendar and Form views to different states, Improving Collapsible buttons
 //2025-06-05 : Adding MealForm, SymptomForm and Calendar, collapsible buttons for the forms
 //2025-06-03 : Creating file and initialising a basic render
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Calendar from './Calendar/Calendar';
-import { ColumnContainer, RowContainer, ScrollableContainer } from '../CustomComponents/CustomComponents';
-import type TrackerDay from '../Types/TrackerDay';
-import {writeToStorage, readFromStorage, clearStorage} from '../Functions/StorageFunctions';
+import { ColumnContainer } from '../CustomComponents/CustomComponents';
 import DayPage from './DayPage/DayPage';
-import MealsContainer from './DayPage/MealsContainer/MealsContainer';
-import SymptomsContainer from './DayPage/SymptomsContainer/SymptomsContainer';
-import type Meal from '../Types/Meal';
-import type Symptom from '../Types/Symptom';
 
 export default function Home() {
-
-  const [trackerData, setTrackerData] = useState<TrackerDay[]>([]); // Placeholder for tracker data
-  const [todayData, setTodayData] = useState<TrackerDay | null>(null);
-  const [selectedDate, setSelectedDate] = useState<TrackerDay | null>(null);
-
-  useEffect(() => {
-    readFromStorage("trackerData").then((data) => {
-      if (data) {
-        
-        const parsedTrackerData : TrackerDay[] = (data as TrackerDay[]).map((day: TrackerDay) => {return {
-          ...day,
-          Date: new Date(day.Date)// Ensure Date is a Date object
-        }});
-        let todayDay : TrackerDay | undefined = parsedTrackerData.find(day => new Date(day.Date).toDateString() === new Date().toDateString())
-        if (!todayDay) {
-          todayDay = {
-            Date: new Date(),
-            Meals: [],
-            Symptoms: [],
-            Notes: ""
-          } as TrackerDay;
-          parsedTrackerData.push(todayDay);
-        }
-        setTodayData(todayDay);
-        setTrackerData(parsedTrackerData);
-      } else {
-        setTrackerData([]); // Initialize with an empty array if no data is found
-      }
-    })
-  }, []);
-
-
-  const handleSubmit = (day : TrackerDay, fromHome : boolean = false) => {
-    const dateIndex = trackerData.findIndex((day : TrackerDay) => day.Date.valueOf() === selectedDate?.Date.valueOf());
-
-    const newTrackerData = [...trackerData];
-
-    if (dateIndex === -1) {
-      // If the date does not exist, create a new TrackerDay
-      newTrackerData.push(day);
-    }
-    else{
-      newTrackerData[dateIndex] = day;
-    }
-    if(!fromHome)
-      setSelectedDate(day); // Update the selected date to the existing TrackerDay
-
-    setTrackerData(newTrackerData);
-    writeToStorage("trackerData", newTrackerData); // Save updated tracker data to storage
-  };
-
-  const calendarDateSelectHandler = (date: Date) => {
-    const selectedTrackerDay = trackerData.find(day => new Date(day.Date).toDateString() === date.toDateString()) ||
-                              {// Create a new TrackerDay if it doesn't exist
-                                      Date: date, // Format date as YYYY-MM-DD
-                                      Meals: [],
-                                      Symptoms: [],
-                                      Notes: ""
-                              } as TrackerDay;
-
-    setSelectedDate(selectedTrackerDay);// Set the selected date to the existing TrackerDay
-  }
-
   return (
-    <ScrollableContainer style={{borderRadius: 0, padding: 0, margin:0, justifyContent:"flex-start", flexDirection:"column", width:"100%"}}>
-      <Text style={{ textAlign:"center", color:"white"}}>Welcome to the Home Screen!</Text>
-      <ColumnContainer style={{display: !selectedDate ? "none" : "flex", flex:1, justifyContent:"flex-start", alignItems:"flex-start"}}>
-        <DayPage selectedDate={selectedDate} backHandler={() => setSelectedDate(null)} submitHandler={handleSubmit} />
-      </ColumnContainer>
-      <ColumnContainer style={{display: selectedDate ? "none" : "flex", flex:1, justifyContent:"flex-start", alignItems:"flex-start", width:"100%"}}>
-        <ScrollableContainer style={{justifyContent:"flex-start"}}>
-          <MealsContainer meals={todayData?.Meals || []} onSubmit={(submitArray : Meal[]) => {
-                  if(todayData === null) return;
-                  const updatedTrackerDay : TrackerDay = {
-                      ...todayData,
-                      Meals:[...submitArray]
-                  };
-                  handleSubmit(updatedTrackerDay, true);
-              }}/>
-          <SymptomsContainer symptoms={todayData?.Symptoms || []} onSubmit={(submitArray : Symptom[]) => {
-                  if(todayData === null) return;
-                  const updatedTrackerDay : TrackerDay = {
-                      ...todayData,
-                      Symptoms:[...submitArray]
-                  };
-                  handleSubmit(updatedTrackerDay, true);
-              }} />
-        </ScrollableContainer>
-        <Calendar setSelectedDate={calendarDateSelectHandler} isVisible={!selectedDate}/>
-      </ColumnContainer>
-    </ScrollableContainer>
+    <ColumnContainer style={{borderRadius: 0, padding: 0, margin:0, justifyContent:"flex-start", flexDirection:"column", width:"100%", height:"100%", paddingTop: 15}}>
+        <DayPage />
+        <Calendar />
+    </ColumnContainer>
   );
 }
 
