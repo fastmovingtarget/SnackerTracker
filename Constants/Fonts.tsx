8da4ee4882cb770b87808966341ae15062f16b05
Target <fastmovingtarget220689@gmail.com>
2025-09-04 12:59:39 +0100

    Fonts object so we can change sizes more easily

---
+++ Constants/Fonts.tsx
@@ -0,0 +1,15 @@
+//2025-09-04 : Fonts object so we can change sizes more easily
+const Fonts = {
+    regular: 'System',
+    bold: 'System-Bold',
+    italic: 'System-Italic',
+    sizes:{
+        mini: 10,
+        small: 13,
+        medium: 15,
+        large: 17,
+        title: 20,
+    }
+};
+
+export default Fonts;
