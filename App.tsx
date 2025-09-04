86cf3f45930ea23a8bd8faac9492e8c2b3c65a50
Target <fastmovingtarget220689@gmail.com>
2025-09-04 12:51:08 +0100

    Adding Tracking Data Provider, Keyboard Avoiding View

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
+//2025-09-04 : Adding Tracking Data Provider, Keyboard Avoiding View
 //2025-08-19 : Styling changes for the main view
 //2025-06-05 : Plugging the home page component into our main app
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
+import TrackerArrayProvider from './Contexts/TrackerContext';
 import Home from './Components/Home';
+import { Colours } from './Constants/Colours';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <TrackerArrayProvider>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Home />
+      </KeyboardAvoidingView>
+    </TrackerArrayProvider>
   );
 }
 
@@ -16,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: '#fff',
+    backgroundColor: Colours.Primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
